cf946c1ae1332428f69ae308fe40352138e7e126
Leão Martins Neto <leao@linksti.com.br>
2024-06-08 17:21:34 -0400

    streams read

--- src/server.js
+++ src/server.js
@@ -1,19 +1,39 @@
 import { createServer } from 'http';
 
 
-const users = [
-    {
-        id: 1,
-        name: 'Alice'
-    },
-
-    { id: 2, name: 'Bob' }
-];
+const users = [];
 
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
 
-    if (req.url === '/users') {
+    const { method, url } = req;
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    try {
+        var body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch (error) {
+        res.writeHead(400).end();
+        return;
+    }
+
+    if (req.method === 'POST' && req.url === '/users') {
+        const { name, email } = body;
+
+        users.push({
+            id: users.length + 1,
+            name,
+            email
+
+        })
+        return res.writeHead(201).end();
+    }
+
+    if (req.method === 'GET' && req.url === '/users') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(users));
